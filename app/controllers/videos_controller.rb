7a82238d2c6a75b4b6fafea3e443002a69743e7b
Iohara Pereira <iohara.pereira@hotmail.com>
2021-08-08 22:56:15 -0300

    Route, controller and new video view added

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,10 @@ class VideosController < ApplicationController
     def index
         @videos = Video.order :nome
     end
+
+    def create
+        video = params.require(:video).permit(:title, :url)
+        Video.create video
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post "videos", to: "videos#create"
+  get "videos/new", to:"videos#new"
   root to:"videos#index"
-
 end
