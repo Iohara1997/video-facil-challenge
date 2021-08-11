10cea2b42ae8619d0a00f86c1e2009c3bd758e32
Iohara Pereira <iohara.pereira@hotmail.com>
2021-08-10 22:58:22 -0300

    Version 1

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,7 @@
 class Video < ApplicationRecord
-
+    has_many :comments
     belongs_to :category, optional: true
 
     validates :url, presence: true
-    validates :title, length: { minimum: 4 }
+    validates :title, presence: true, length: { minimum: 4 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
   resources :categories
   get "videos/search", to: "videos#search"
-  resources :videos, only: [:new, :create, :destroy, :edit, :update, :show]
   root to:"videos#index"
+  resources :videos, only: [:new, :create, :destroy, :edit, :update, :show] do
+    resources :comments
+  end
+  post "videos/:video_id/comments/new", to: "comments#create"
 end
